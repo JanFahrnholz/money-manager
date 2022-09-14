e8beab9de1bf4148462323042014957194ed71fd
Jan <jf@ambimax.de>
2022-09-14 09:33:20 +0200

    fix quickstats header

--- components/misc/StatsHeader.tsx
+++ components/misc/StatsHeader.tsx
@@ -5,26 +5,25 @@ import { TransactionContext } from "../../context/TransactionContext";
 const StatsHeader: FC = () => {
     const ctx = useContext(ProfileContext);
 
-    const stats = [
-        { name: "Balance", stat: ctx.profile.balance },
-        { name: "Pending", stat: ctx.profile.pending },
-    ];
     return (
         <>
             <dl className="grid grid-flow-col gap-2 p-2">
-                {stats.map((item) => (
-                    <div
-                        key={item.name}
-                        className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
-                    >
-                        <dt className="truncate text-sm font-medium text-gray-500">
-                            {item.name}
-                        </dt>
-                        <dd className="m-0 mt-1 text-2xl font-semibold tracking-tight text-gray-900 text-left">
-                            {item.stat}€
-                        </dd>
-                    </div>
-                ))}
+                {ctx.stats.statistics.map((item, i) => {
+                    if (i >= 2) return;
+                    return (
+                        <div
+                            key={item.name}
+                            className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+                        >
+                            <dt className="truncate text-sm font-medium text-gray-500">
+                                {item.name}
+                            </dt>
+                            <dd className="m-0 mt-1 text-2xl font-semibold tracking-tight text-gray-900 text-left">
+                                {item.value()}€
+                            </dd>
+                        </div>
+                    );
+                })}
             </dl>
         </>
     );
