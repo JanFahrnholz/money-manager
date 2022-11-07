5324418bec11c25933b353c958d2931c1a10d950
Jan <jf@ambimax.de>
2022-11-07 15:23:33 +0100

    improve transaction list performance

--- src/components/transactions/list/index.tsx
+++ src/components/transactions/list/index.tsx
@@ -1,12 +1,15 @@
 import { CircularProgress, List, ListSubheader } from "@mui/material";
-import { FC, useContext, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useRef, useState } from "react";
+import ViewportList from "react-viewport-list";
 import { TransactionContext } from "../../../context/TransactionContext";
 import Transaction from "../../../types/Transaction";
 import TransactionListItem from "./item";
 
 const TransactionList: FC = () => {
     const storage = useContext(TransactionContext);
-    const [transactions, setTransactions] = useState<Transaction[][]>(null!);
+    const [transactions, setTransactions] = useState<Transaction[][]>(
+        Object.values(storage.getSortedTransactions())
+    );
 
     useEffect(() => {
         const t = Object.values(storage.getSortedTransactions());
@@ -21,26 +24,26 @@ const TransactionList: FC = () => {
 
     return (
         <div className="p-2">
-            {transactions !== null && (
-                <List
-                    sx={{
-                        width: "100%",
-                        maxHeight: "80vh",
-                        bgcolor: "background.default",
-                        position: "relative",
-                        overflow: "auto",
-                        "& ul": { padding: 0 },
-                    }}
-                    subheader={<li />}
-                >
-                    {transactions.map((month) => (
-                        <li key={`section-${month[0].date}`}>
+            <List
+                sx={{
+                    width: "100%",
+                    maxHeight: "80vh",
+                    bgcolor: "background.default",
+                    position: "relative",
+                    overflow: "auto",
+                    "& ul": { padding: 0 },
+                }}
+                subheader={<li />}
+            >
+                <ViewportList items={transactions} itemMinSize={20}>
+                    {(item) => (
+                        <li key={`section-${item[0].date}`}>
                             <ul>
                                 <ListSubheader sx={{ borderRadius: "5px" }}>
-                                    {formatDate(month[0].date)}
+                                    {formatDate(item[0].date)}
                                 </ListSubheader>
 
-                                {month.map((transaction: Transaction) => (
+                                {item.map((transaction: Transaction) => (
                                     <TransactionListItem
                                         key={transaction.id}
                                         transaction={transaction}
@@ -48,17 +51,28 @@ const TransactionList: FC = () => {
                                 ))}
                             </ul>
                         </li>
-                    ))}
-                </List>
-            )}
-
-            {transactions === null && (
-                <div className="text-center">
-                    <CircularProgress />
-                </div>
-            )}
+                    )}
+                </ViewportList>
+            </List>
         </div>
     );
 };
 
 export default TransactionList;
+
+// {transactions.map((month) => (
+//     <li key={`section-${month[0].date}`}>
+//         <ul>
+//             <ListSubheader sx={{ borderRadius: "5px" }}>
+//                 {formatDate(month[0].date)}
+//             </ListSubheader>
+
+//             {month.map((transaction: Transaction) => (
+//                 <TransactionListItem
+//                     key={transaction.id}
+//                     transaction={transaction}
+//                 />
+//             ))}
+//         </ul>
+//     </li>
+// ))}
