a2cfd589a1dafb4f99f1bda25bcc1db0119a8b71
JanFahrnholz <jm.fahrnholz@gmail.com>
2022-12-21 21:47:29 +0100

    add user balance, add handler when transaction created/deleted

--- src/components/misc/StatsHeader.tsx
+++ src/components/misc/StatsHeader.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 import useUser from "hooks/useUser";
+import { modifyBalance } from "lib/User";
 import { FC, useContext, useMemo } from "react";
 import { ContactContext } from "../../context/ContactContext";
 import { TransactionContext } from "../../context/TransactionContext";
@@ -7,65 +8,67 @@ import { getMoneyToPayBack, getPendingMoney } from "../../lib/Statistics";
 import LoadValue from "./LoadValue";
 
 const StatsHeader: FC = () => {
-    const user = useUser();
-    const { contacts } = useContext(ContactContext);
-    const { transactions } = useContext(TransactionContext);
+	const { contacts } = useContext(ContactContext);
+	const { transactions } = useContext(TransactionContext);
 
-    const pendingMoney = useMemo(() => getPendingMoney(contacts), [contacts]);
-    const toPay = useMemo(() => getMoneyToPayBack(contacts), [contacts]);
+	const user = useUser();
+	console.log(user);
 
-    return (
-        <Grid container spacing={1} p={1}>
-            {/* <Grid item xs={4}>
-                <Card>
-                    <CardContent>
-                        <Typography
-                            sx={{ fontSize: 14 }}
-                            color="text.secondary"
-                            gutterBottom
-                        >
-                            balance
-                        </Typography>
-                        <Typography variant="h5">
-                            <LoadValue value={user?.balance} />€
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid> */}
-            <Grid item xs={6}>
-                <Card>
-                    <CardContent>
-                        <Typography
-                            sx={{ fontSize: 14 }}
-                            color="text.secondary"
-                            gutterBottom
-                        >
-                            pending
-                        </Typography>
-                        <Typography variant="h5">
-                            <LoadValue value={pendingMoney} />€
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={6}>
-                <Card>
-                    <CardContent>
-                        <Typography
-                            sx={{ fontSize: 14 }}
-                            color="text.secondary"
-                            gutterBottom
-                        >
-                            to pay
-                        </Typography>
-                        <Typography variant="h5">
-                            <LoadValue value={toPay} />€
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-    );
+	const pendingMoney = useMemo(() => getPendingMoney(contacts), [contacts]);
+	const toPay = useMemo(() => getMoneyToPayBack(contacts), [contacts]);
+
+	return (
+		<Grid container spacing={1} p={1}>
+			<Grid item xs={4}>
+				<Card>
+					<CardContent>
+						<Typography
+							sx={{ fontSize: 14 }}
+							color="text.secondary"
+							gutterBottom
+						>
+							balance
+						</Typography>
+						<Typography variant="h5">
+							<LoadValue value={user?.balance} />€
+						</Typography>
+					</CardContent>
+				</Card>
+			</Grid>
+			<Grid item xs={4}>
+				<Card>
+					<CardContent>
+						<Typography
+							sx={{ fontSize: 14 }}
+							color="text.secondary"
+							gutterBottom
+						>
+							pending
+						</Typography>
+						<Typography variant="h5">
+							<LoadValue value={pendingMoney} />€
+						</Typography>
+					</CardContent>
+				</Card>
+			</Grid>
+			<Grid item xs={4}>
+				<Card>
+					<CardContent>
+						<Typography
+							sx={{ fontSize: 14 }}
+							color="text.secondary"
+							gutterBottom
+						>
+							to pay
+						</Typography>
+						<Typography variant="h5">
+							<LoadValue value={toPay} />€
+						</Typography>
+					</CardContent>
+				</Card>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default StatsHeader;
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,11 +1,42 @@
+import User from "@/types/User";
 import { client } from "lib/Pocketbase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useTrigger from "./useTrigger";
 
 const useUser = () => {
-	const [user, setUser] = useState(client.authStore.model);
+	const [user, setUser] = useState<User>();
+	const [trigger, reload] = useTrigger();
+	const id = client.authStore.model?.id;
+
+	useEffect(() => {
+		if (!id) return;
+		fetchUser(id);
+
+		return () => unsubscribe();
+	}, [trigger]);
+
+	const fetchUser = async (id: string) => {
+		try {
+			const user = await client
+				.collection("users")
+				.getOne<User>(id, { $cancelKey: "fetchUser" });
+
+			setUser(user);
+		} catch (error) {}
+	};
+	if (!id) return;
+
+	client.collection("users").subscribe(id, () => fetchUser(id));
+
+	const unsubscribe = () => {
+		client
+			.collection("users")
+			.unsubscribe(id)
+			.catch(() => {});
+	};
 
 	client.authStore.onChange((auth) => {
-		setUser(client.authStore.model);
+		reload();
 	});
 
 	return user;
