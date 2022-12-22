c0253be13ce0cd59f135160e3772dd988ee60a3f
Jan <jf@ambimax.de>
2022-12-22 16:06:18 +0100

    added info

--- src/components/transactions/list/planned-item.tsx
+++ src/components/transactions/list/planned-item.tsx
@@ -46,64 +46,64 @@ const PlannedTransactionListItem: FC<{ transaction: Record<Transaction> }> = ({
 		);
 	};
 
-	return (
-		<ListItem
-			key={transaction.id}
-			alignItems="flex-start"
-			sx={{
-				my: 1,
-				borderRadius: 1,
-				bgcolor: "secondary.main",
-				color: "secondary.contrastText",
-			}}
-			className="shadow border"
-			secondaryAction={secondaryAction()}
-		>
-			<ListItemAvatar>
-				<Avatar
-					sx={{
-						bgcolor: getColor(transaction.type),
-					}}
-				>
-					{isOwner ? (
-						<>{getInitials(transaction.expand.contact.name)}</>
-					) : (
-						<>
-							<LinkIcon />
-						</>
-					)}
-				</Avatar>
-			</ListItemAvatar>
-			<ListItemText
-				primary={`${transaction.type} ${transaction.amount}€`}
-				secondary={
-					<Typography
-						sx={{
-							display: "inline",
-						}}
-						component="span"
-						variant="body2"
-					>
-						{isOwner ? (
-							<>
-								{transaction.expand.contact.name}
-								{transaction.expand.contact.user && (
-									<span className="pl-1">🔗</span>
-								)}
-							</>
-						) : (
-							<LinkedFrom txt={transaction.owner} />
-						)}
-					</Typography>
-				}
-			/>
-			<TransactionDetailMenu
-				transaction={transaction}
-				open={openActions}
-				setOpen={setOpenActions}
-			/>
-		</ListItem>
-	);
+    return (
+        <ListItem
+            key={transaction.id}
+            alignItems="flex-start"
+            sx={{
+                my: 1,
+                borderRadius: 1,
+                bgcolor: "secondary.main",
+                color: "secondary.contrastText",
+            }}
+            className="shadow border"
+            secondaryAction={secondaryAction()}
+        >
+            <ListItemAvatar>
+                <Avatar
+                    sx={{
+                        bgcolor: getColor(transaction.type),
+                    }}
+                >
+                    {isOwner ? (
+                        <>{getInitials(transaction.expand.contact.name)}</>
+                    ) : (
+                        <>
+                            <LinkIcon />
+                        </>
+                    )}
+                </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+                primary={`${transaction.type} ${transaction.amount}€ - ${transaction.info}`}
+                secondary={
+                    <Typography
+                        sx={{
+                            display: "inline",
+                        }}
+                        component="span"
+                        variant="body2"
+                    >
+                        {isOwner ? (
+                            <>
+                                {transaction.expand.contact.name}
+                                {transaction.expand.contact.user && (
+                                    <span className="pl-1">🔗</span>
+                                )}
+                            </>
+                        ) : (
+                            <LinkedFrom txt={transaction.owner} />
+                        )}
+                    </Typography>
+                }
+            />
+            <TransactionDetailMenu
+                transaction={transaction}
+                open={openActions}
+                setOpen={setOpenActions}
+            />
+        </ListItem>
+    );
 };
 
 export default PlannedTransactionListItem;
