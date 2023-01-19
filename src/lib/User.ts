bd33882c87f76adb0d7c70812a3822c898ef706a
Jan <jf@ambimax.de>
2023-01-19 10:41:49 +0100

    fix user balance when Rückzahlung is confirmed

--- src/lib/User.ts
+++ src/lib/User.ts
@@ -7,35 +7,37 @@ import toast from "react-hot-toast";
 import { client } from "./Pocketbase";
 
 const modifyBalance = async (modifier: number) => {
-	const id = client.authStore.model?.id;
-	if (!id) return;
-	try {
-		const contact = await client.collection("users").getOne(id);
+    const id = client.authStore.model?.id;
+    if (!id) return;
+    try {
+        const contact = await client.collection("users").getOne(id);
 
-		await update(id, { balance: contact.balance + modifier });
-	} catch (error) {}
+        await update(id, { balance: contact.balance + modifier });
+    } catch (error) {}
 };
 
 const update = async (id: string, data: Partial<User>) => {
-	try {
-		return await client.collection("users").update(id, data);
-	} catch (error) {
-		toast.error("could not update user");
-	}
+    try {
+        return await client.collection("users").update(id, data);
+    } catch (error) {
+        toast.error("could not update user");
+    }
 };
 
 const updateBalanceOnCreate = async (transaction: TransactionRecord) => {
-	const { amount, type } = transaction;
+    const { amount, type } = transaction;
 
-	if (type === "Einnahme") await modifyBalance(amount);
-	if (type === "Ausgabe") await modifyBalance(-amount);
+    if (type === "Einnahme") await modifyBalance(amount);
+    if (type === "Rückzahlung") await modifyBalance(amount);
+    if (type === "Ausgabe") await modifyBalance(-amount);
 };
 
 const updateBalanceOnDelete = async (transaction: TransactionRecord) => {
-	const { type, amount } = transaction;
+    const { type, amount } = transaction;
 
-	if (type === "Einnahme") await modifyBalance(-amount);
-	if (type === "Ausgabe") await modifyBalance(amount);
+    if (type === "Einnahme") await modifyBalance(-amount);
+    if (type === "Rückzahlung") await modifyBalance(-amount);
+    if (type === "Ausgabe") await modifyBalance(amount);
 };
 
 export { update, modifyBalance, updateBalanceOnCreate, updateBalanceOnDelete };
