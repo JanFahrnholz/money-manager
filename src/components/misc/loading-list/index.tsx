a0d1471b9c6caa4c27138c250d3b9208f168b868
Jan <jf@ambimax.de>
2023-01-19 13:44:07 +0100

    create lazy loading list for transactions

--- src/components/misc/loading-list/index.tsx
+++ src/components/misc/loading-list/index.tsx
@@ -1,61 +1,102 @@
-import { FC, useState } from "react";
-import { InfiniteLoader, List } from "react-virtualized";
+import TransactionListItem from "@/components/transactions/list/item";
+import TransactionRecord from "@/types/TransactionRecord";
+import { Box, Divider, ListSubheader, Typography } from "@mui/material";
+import { formatMonthlyDate, formatDailyDate } from "lib/Formatter";
+import { client } from "lib/Pocketbase";
+import { useEffect, useRef, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
+import LoadValue from "../LoadValue";
+import RenderInterval from "../RenderInterval";
 
-interface Props {
-    hasNextPage: boolean;
-    isNextPageLoading: boolean;
-    list: any[];
-    loadNextPage: Promise<any[]>;
-}
+const TransactionLoadingList = () => {
+    const [data, setData] = useState<TransactionRecord[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(false);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const perPage = 10;
+    const ref = useRef();
 
-const LoadingList: FC<Props> = ({
-    hasNextPage,
-    isNextPageLoading,
-    list,
-    loadNextPage,
-}) => {
-    // If there are more items to be loaded then add an extra row to hold a loading indicator.
-    const rowCount = hasNextPage ? list.length + 1 : list.length;
+    useEffect(() => {
+        fetchData();
+    }, [page]);
 
-    // Only load 1 page of items at a time.
-    // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-    const loadMoreRows = isNextPageLoading ? () => {} : loadNextPage;
-
-    // Every row is loaded except for our loading indicator row.
-    const isRowLoaded = ({ index }) => !hasNextPage || index < list.length;
-
-    // Render a list item or a loading indicator.
-    const rowRenderer = ({ index, key, style }) => {
-        let content;
-
-        if (!isRowLoaded({ index })) {
-            content = "Loading...";
-        } else {
-            content = list.getIn([index, "name"]);
+    async function fetchData() {
+        setIsLoading(true);
+        try {
+            const response = await client
+                .collection("transactions")
+                .getList<TransactionRecord>(page, perPage, {
+                    sort: "-date",
+                    expand: "contact,owner",
+                });
+            setData([...data, ...response.items]);
+            setIsLoading(false);
+            setHasMore(response.page < response.totalPages);
+            setTotalPages(response.totalPages);
+        } catch (err) {
+            console.log(err);
+            setIsLoading(false);
         }
+    }
 
-        return (
-            <div key={key} style={style}>
-                {content}
-            </div>
-        );
+    const loadMoreRows = () => {
+        if (hasMore) setPage(page + 1);
     };
 
     return (
-        <InfiniteLoader
-            isRowLoaded={isRowLoaded}
-            loadMoreRows={loadMoreRows}
-            rowCount={rowCount}
-        >
-            {({ onRowsRendered, registerChild }) => (
-                <List
-                    ref={registerChild}
-                    onRowsRendered={onRowsRendered}
-                    rowRenderer={rowRenderer}
-                />
-            )}
-        </InfiniteLoader>
+        <div className="px-2">
+            <Virtuoso
+                style={{ height: "calc(80vh - 50px)", paddingBottom: "50px" }}
+                data={data}
+                endReached={() => loadMoreRows()}
+                itemContent={(index) => (
+                    <>
+                        <RenderInterval
+                            array={data}
+                            index={index}
+                            monthly={(date) =>
+                                subHeader(formatMonthlyDate(date))
+                            }
+                            daily={(date) => dayDivider(date)}
+                        />
+                        <TransactionListItem transaction={data[index]} />
+                    </>
+                )}
+            />
+            {isLoading && <p>loading</p>}
+        </div>
     );
 };
 
-export default LoadingList;
+export default TransactionLoadingList;
+
+const subHeader = (text: string) => {
+    return (
+        <Box
+            sx={{
+                textAlign: "center",
+                backgroundColor: "background.paper",
+                zIndex: 1000,
+                borderRadius: "5px",
+                p: 1.5,
+            }}
+        >
+            {text}
+        </Box>
+    );
+};
+
+const dayDivider = (date: Date) => (
+    <Divider
+        sx={{
+            width: "75%",
+            mx: "auto",
+            mt: 1,
+        }}
+    >
+        <Typography sx={{ color: "text.secondary", fontSize: 14 }}>
+            {formatDailyDate(date)}
+        </Typography>
+    </Divider>
+);
