97120feff20cd552ab66429d17ea88823aa83cfc
Jan <jf@ambimax.de>
2023-01-29 14:32:30 +0100

    add user profile fetching

--- src/features/user-profiles/hooks/useProfile.ts
+++ src/features/user-profiles/hooks/useProfile.ts
@@ -1,10 +1,17 @@
+import { client } from "lib/Pocketbase";
 import { useContext } from "react";
 import { ProfileContext } from "../context";
 
-const useProfile = () => {
+const useProfile = (userId?: string) => {
     const { profile } = useContext(ProfileContext);
 
-    return profile;
+    const getProfile = async (id: string) => {
+        await client.collection("profiles").getOne(id);
+    };
+
+    if (!userId) return profile;
+
+    return getProfile(userId);
 };
 
 export default useProfile;
