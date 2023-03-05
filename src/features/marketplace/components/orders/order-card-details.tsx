5bd345da52b5e98dc092f615f82b00f06dd10190
JanFahrnholz <jm.fahrnholz@gmail.com>
2023-03-05 16:51:28 +0100

    fix order quantity display

--- src/features/marketplace/components/orders/order-card-details.tsx
+++ src/features/marketplace/components/orders/order-card-details.tsx
@@ -6,44 +6,44 @@ import { ProductRecord } from "features/marketplace/types/Product";
 import { client } from "lib/Pocketbase";
 import { FC } from "react";
 interface Props {
-    order: OrderRecord;
+	order: OrderRecord;
 }
 const OrderCardDetails: FC<Props> = ({ order }) => {
-    const product = order.expand.product as ProductRecord;
-    const contact = order.expand.contact as Contact;
-    const id = client.authStore.model?.id;
-    const isOwner = product.owner === id;
+	const product = order.expand.product as ProductRecord;
+	const contact = order.expand.contact as Contact;
+	const id = client.authStore.model?.id;
+	const isOwner = product.owner === id;
 
-    const orderFrom = isOwner ? (
-        <>
-            order from {contact.name}{" "}
-            <LinkedFrom owner={contact.user} asText disableId />
-        </>
-    ) : (
-        <>
-            your order at <LinkedFrom owner={product.owner} asText />
-        </>
-    );
+	const orderFrom = isOwner ? (
+		<>
+			order from {contact.name}{" "}
+			<LinkedFrom owner={contact.user} asText disableId />
+		</>
+	) : (
+		<>
+			your order at <LinkedFrom owner={product.owner} asText />
+		</>
+	);
 
-    return (
-        <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                {orderFrom}
-            </Typography>
-            <Typography variant="h5" component="div" sx={{ my: 1 }}>
-                {product.name} {order.quantity}
-                {product.unit}
-            </Typography>
-            <Typography variant="body2">
-                Price: {order.quantity * product.price}€ ({product.price}€/
-                {product.unit}) {!order.payDirectly && "not payed directly"}
-            </Typography>
+	return (
+		<CardContent>
+			<Typography sx={{ fontSize: 14 }} color="text.secondary">
+				{orderFrom}
+			</Typography>
+			<Typography variant="h5" component="div" sx={{ my: 1 }}>
+				{product.name} {order.quantity.toFixed(2)}
+				{product.unit}
+			</Typography>
+			<Typography variant="body2">
+				Price: {order.quantity * product.price}€ ({product.price}€/
+				{product.unit}) {!order.payDirectly && "not payed directly"}
+			</Typography>
 
-            <Typography variant="body1" sx={{ mt: 1 }}>
-                {order.message}
-            </Typography>
-        </CardContent>
-    );
+			<Typography variant="body1" sx={{ mt: 1 }}>
+				{order.message}
+			</Typography>
+		</CardContent>
+	);
 };
 
 export default OrderCardDetails;
