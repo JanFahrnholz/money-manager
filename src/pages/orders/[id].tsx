d965598adc36d9f7beaafda55b3fbac2f2e9e2d9
Jan <jf@ambimax.de>
2023-03-09 23:53:18 +0100

    add order details page

--- src/pages/orders/[id].tsx
+++ src/pages/orders/[id].tsx
@@ -1,26 +1,49 @@
+import SubSiteHeader from "@/components/misc/SubSiteHeader";
+import { Button, CssBaseline } from "@mui/material";
+import ChatComponent from "features/chats/components/chat";
+import useChat from "features/chats/hooks/useChat";
 import { OrderRecord } from "features/marketplace/types/Order";
 import { client } from "lib/Pocketbase";
-import {
-	GetServerSideProps,
-	GetStaticPaths,
-	GetStaticProps,
-	NextPage,
-} from "next";
+import { NextPage } from "next";
 import { NextRouter, useRouter } from "next/router";
-import { ParsedUrlQuery } from "querystring";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { Toaster } from "react-hot-toast";
+import Orders from ".";
 
 const OrderDetailsPage: NextPage = () => {
-	const router: NextRouter = useRouter();
-	const { id } = router.query as { id: string };
+    const [order, setOrder] = useState<OrderRecord | undefined>();
+    const router: NextRouter = useRouter();
+    const query = router.query as { id: string };
 
-	const order = useMemo(async () => {
-		const order = await client.collection("orders").getOne(id);
-	}, [id]);
+    useEffect(() => {
+        const fetch = async () => {
+            if (!query.id) return;
+            try {
+                const order = await client
+                    .collection("orders")
+                    .getOne<OrderRecord>(query.id);
+                setOrder(order);
+            } catch (error) {}
+        };
+        fetch();
+    }, [query.id]);
 
-	if (!order) return <>error no order</>;
+    if (!order) return <>error no order</>;
 
-	return <>order details {order}</>;
+    return (
+        <>
+            <CssBaseline />
+            <SubSiteHeader title={`order details`}>
+                {/* order details {order.quantity} */}
+                <ChatComponent id={order.chat} />
+            </SubSiteHeader>
+            <Toaster
+                toastOptions={{
+                    duration: 2000,
+                }}
+            />
+        </>
+    );
 };
 
 export default OrderDetailsPage;
