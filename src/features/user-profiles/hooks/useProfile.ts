ea7758640b6d80d2d4b7652e0d407c40ba15fbbe
Jan <jf@ambimax.de>
2023-03-09 23:52:54 +0100

    add error message

--- src/features/user-profiles/hooks/useProfile.ts
+++ src/features/user-profiles/hooks/useProfile.ts
@@ -1,15 +1,19 @@
 import Profile from "@/types/Profile";
 import { client } from "lib/Pocketbase";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ProfileContext } from "../context";
 
 const useProfile = () => {
     const { profile } = useContext(ProfileContext);
 
     const get = async (id: string) => {
-        return await client
-            .collection("profiles")
-            .getFirstListItem<Profile>(`user.id="${id}"`);
+        try {
+            return await client
+                .collection("profiles")
+                .getFirstListItem<Profile>(`user.id="${id}"`);
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
     };
 
     return { profile, get };
