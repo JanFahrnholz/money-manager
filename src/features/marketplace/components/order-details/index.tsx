c8772af895729b3af6b34aba699d2b2fb15ce939
Jan <jf@ambimax.de>
2023-03-20 12:20:12 +0100

    use onChange callback for swipeable views

--- src/features/marketplace/components/order-details/index.tsx
+++ src/features/marketplace/components/order-details/index.tsx
@@ -2,13 +2,11 @@ import SwipeableViews from "@/components/swipeable-views";
 import SwipeableViewsDefaultTabs from "@/components/swipeable-views/default-tabs";
 import Slide from "@/components/swipeable-views/slide";
 import LockIcon from "@mui/icons-material/Lock";
-import { Button, Typography } from "@mui/material";
 import ChatComponent from "features/chats/components/chat";
-import useChat from "features/chats/hooks/useChat";
 import { OrderRecord } from "features/marketplace/types/Order";
 import useSetting from "features/user-settings/hooks/useSetting";
-import { FC } from "react";
-import ReactTimeAgo from "react-time-ago";
+import { FC, useState } from "react";
+import OrderActionsMenu from "../misc/order-actions-menu";
 import OrderCardStatus from "../orders/order-card-status";
 import OrderDetailsTabContent from "./order-details";
 interface OrderDetailsPageProps {
@@ -17,6 +15,7 @@ interface OrderDetailsPageProps {
 
 const OrderDetails: FC<OrderDetailsPageProps> = ({ order }) => {
     const chatsEnabled: boolean = useSetting("enableChats");
+    const [active, setActive] = useState(true);
 
     const tabs: Slide[] = [
         {
@@ -35,8 +34,10 @@ const OrderDetails: FC<OrderDetailsPageProps> = ({ order }) => {
     return (
         <>
             <OrderCardStatus order={order} horizontal />
+            <OrderActionsMenu order={order} active={active} />
             <SwipeableViews
                 slides={tabs}
+                onChange={(activeIndex) => setActive(activeIndex === 0)}
                 tabs={(slides, currentSlide, setCurrentSlide) => (
                     <SwipeableViewsDefaultTabs
                         slides={slides}
