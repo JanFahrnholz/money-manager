09eb906a9c205871a0aec140b00c0aee5ae773b1
Jan <jf@ambimax.de>
2023-05-18 17:13:51 +0200

    send message on enter

--- src/features/chats/components/chat/chat-input.tsx
+++ src/features/chats/components/chat/chat-input.tsx
@@ -1,6 +1,6 @@
 import { FormControl, OutlinedInput, Paper } from "@mui/material";
 import Chat from "features/chats/types/chat";
-import { FC, useState } from "react";
+import { FC, KeyboardEvent, useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import useChat from "features/chats/hooks/useChat";
 
@@ -16,11 +16,18 @@ const ChatInput: FC<ChatInputProps> = ({}) => {
             setMessage("");
         } catch (error) {}
     };
+
+    const handleKeydown = (event: KeyboardEvent) => {
+        if (event.code === "Enter") sendMessage();
+    };
+
     return (
         <FormControl variant="filled" fullWidth>
             <OutlinedInput
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
+                onClick={() => sendMessage()}
+                onKeyDown={(e) => handleKeydown(e)}
                 placeholder="message..."
                 endAdornment={<SendIcon onClick={() => sendMessage()} />}
             />
